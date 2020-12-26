0963ac2aa5ae92d4c741519f32fc5926445744c3
sadiashraful <saditech18@gmail.com>
2020-12-26 16:22:31 +0600

    Started a new project: lootlogger

---
+++ IOS Projects/LootLogger/ItemsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Mohammad Ashraful Islam Sadi on 24/12/20.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    var itemStore: ItemStore!
+    
+}
---
+++ IOS Projects/LootLogger/LootLogger/Item.swift
@@ -0,0 +1,42 @@
+//
+//  Item.swift
+//  LootLogger
+//
+//  Created by Mohammad Ashraful Islam Sadi on 24/12/20.
+//
+
+import UIKit
+
+class Item {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.serialNumber = serialNumber
+        self.valueInDollars = valueInDollars
+        self.dateCreated = Date()
+    }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            let randomAdjective = adjectives.randomElement()!
+            let randomNoun = nouns.randomElement()!
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int.random(in: 0..<100)
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName,
+                      serialNumber: randomSerialNumber,
+                      valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
+}
